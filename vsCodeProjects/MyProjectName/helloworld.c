9484ab10e0638d130290a34a7a332b23b9f70817
malak mas <malakmasarwe669@gmail.com>
2024-09-15 22:49:16 +0300

    Remove accidentally pushed files

--- vsCodeProjects/MyProjectName/helloworld.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "myheader.h"
-
-int main()
-{
-    printf("Change the world");
-    return 0;
-
-}
--- vsCodeProjects/New folder/out/build/pex/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/malak/vsCodeProjects/New folder
-# Build directory: C:/Users/malak/vsCodeProjects/New folder/out/build/pex
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
